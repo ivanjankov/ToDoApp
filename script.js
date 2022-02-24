d1aa255a0cf60c19b9a1920a9a1df74150b09239
Ivan Jankov <ivanjankov07@gmail.com>
2022-02-24 12:44:26 +0100

    create appendTaskList function

--- script.js
+++ script.js
@@ -6,6 +6,30 @@ const addTaskBtn = document.getElementById('add-btn');
 
 // apeend new task in taskwrapper element
 
+addTaskBtn.addEventListener('click', appendTaskList);
+
+function appendTaskList() {
+	let data = getValuesFromInputs();
+	if (data.name !== '' && data.date != '') {
+		let newTask = document.createElement('div');
+		newTask.classList.add('single-task');
+		newTask.innerHTML = `
+                            <label class="checkmark-container">
+                                <input type="checkbox" checked="${false}" />
+                                <span class="checkmark"></span>
+                            </label>
+                            <div class="task-name">
+                                <p>${data.name}</p>
+                            </div>
+                            <div class="task-date">
+                                <p>${data.date}</p>
+                            </div>
+        `;
+
+		taskWrapper.appendChild(newTask);
+		resetInputFields();
+	}
+}
 function getValuesFromInputs() {
 	return {
 		name: nameInput.value,
