0f7f3be7b7eeb9c3756b8444982f5adb02db5880
Ivan Jankov <ivanjankov07@gmail.com>
2022-02-24 12:43:55 +0100

    Create getValues function

--- script.js
+++ script.js
@@ -3,3 +3,13 @@ const nameInput = document.getElementById('name-input');
 const dateInput = document.getElementById('date-input');
 const statusInput = document.getElementById('status-input');
 const addTaskBtn = document.getElementById('add-btn');
+
+// apeend new task in taskwrapper element
+
+function getValuesFromInputs() {
+	return {
+		name: nameInput.value,
+		date: dateInput.value,
+		status: statusInput.checked,
+	};
+}
