325cf360f539cec82b03e9e419087fdb8ab58d4c
Ivan Jankov <ivanjankov07@gmail.com>
2022-02-24 12:41:10 +0100

    Add id's and select elements from task wrapper

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
-
+let taskWrapper = document.getElementById('task-wrapper');
+const nameInput = document.getElementById('name-input');
+const dateInput = document.getElementById('date-input');
+const statusInput = document.getElementById('status-input');
+const addTaskBtn = document.getElementById('add-btn');
