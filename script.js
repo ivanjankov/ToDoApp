b0d3beb56ff3494dc402c3ff631ccebe5c64b16c
Ivan Jankov <ivanjankov07@gmail.com>
2022-02-24 16:13:43 +0100

    Create createTask fn, add eventlistener to delete btns,

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const statusInput = document.getElementById('status-input');
 const addTaskBtn = document.getElementById('add-btn');
 
 // apeend new task in taskwrapper element
-let toDoItems = [];
+let toDoTasks = [];
 let idCounter = 1;
 
 addTaskBtn.addEventListener('click', appendTaskList);
@@ -13,28 +13,11 @@ addTaskBtn.addEventListener('click', appendTaskList);
 function appendTaskList() {
 	let data = getValuesFromInputs();
 	if (data.name !== '' && data.date != '') {
-		toDoItems.push(data);
-		let newTask = document.createElement('div');
-		newTask.classList.add('single-task');
-		newTask.setAttribute('id', data.id);
-		newTask.innerHTML = `
-							<label class="checkmark-container">
-								<input type="checkbox" checked="${false}" />
-								<span class="checkmark"></span>
-							</label>
-							<div class="task-name">
-								<p>${data.name}</p>
-							</div>
-							<div class="task-date">
-								<p>${data.date}</p>
-							</div>
-							<div class="task-date">
-									<a href="#" class="edit-task">Edit</a>
-									<a href="#" class="delete-task">Delete</a>
-							</div>
-				`;
+		toDoTasks.push(data);
+		let newTask = createTaskElement(data);
 
 		taskWrapper.appendChild(newTask);
+		addEventListenerToDeleteBtn();
 		resetInputFields();
 	}
 }
@@ -51,4 +34,38 @@ function resetInputFields() {
 	nameInput.value = '';
 	dateInput.value = '';
 	statusInput.checked = false;
+}
+
+function addEventListenerToDeleteBtn() {
+	let btnsArr = Array.from(document.querySelectorAll('.delete-task'));
+	btnsArr.forEach((btn) => {
+		btn.addEventListener('click', deleteTask);
+	});
+}
+
+function updateTaskListOnChange(array) {
+	toDoTasks = array;
+}
+
+function createTaskElement(data) {
+	let newTask = document.createElement('div');
+	newTask.classList.add('single-task');
+	newTask.setAttribute('id', data.id);
+	newTask.innerHTML = `
+						<label class="checkmark-container">
+							<input type="checkbox" checked="${false}" />
+							<span class="checkmark"></span>
+						</label>
+						<div class="task-name">
+							<p>${data.name}</p>
+						</div>
+						<div class="task-date">
+							<p>${data.date}</p>
+						</div>
+						<div class="task-date">
+								<a href="#" class="edit-task">Edit</a>
+								<a href="#" class="delete-task">Delete</a>
+						</div>
+			`;
+	return newTask;
 }
